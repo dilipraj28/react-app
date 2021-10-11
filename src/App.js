c2ab21e6aa80545b5e869dce6485a00625698e73
dilipraj <s.champ2360@gmail.com>
2021-10-11 19:51:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import React from "react";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="main">
+      <div className="box">
+        <img
+          src="https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/Zugpsitze_mountain.jpg?crop=0,176,3008,1654&wid=4000&hei=2200&scl=0.752"
+          alt="nature"
+        ></img>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="vert">
+        <div className="box boxV">
+          <img
+          src="https://www.iucn.org/sites/dev/files/content/images/2020/shutterstock_1458128810.jpg"
+          alt="nature"
+        ></img>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        </div>
+        <div className="box boxV">
+          <img
+          src="https://www.iucn.org/sites/dev/files/content/images/2020/shutterstock_1458128810.jpg"
+          alt="nature"
+        ></img>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        </div>
+        <div className="box boxV">
+          <img
+          src="https://www.iucn.org/sites/dev/files/content/images/2020/shutterstock_1458128810.jpg"
+          alt="nature"
+        ></img>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+        </div>
+      </div>
+      <div className="box">
+        <img
+          src="https://www.iucn.org/sites/dev/files/content/images/2020/shutterstock_1458128810.jpg"
+          alt="nature"
+        ></img>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+      </div>
+      <div className="box">
+        <img
+          src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?size=626&ext=jpg"
+          alt="nature"
+        ></img>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </p>
+      </div>
+      {/* <div>
+        <img src="https://pi.tedcdn.com/r/talkstar-assets.s3.amazonaws.com/production/playlists/playlist_398/reconnect_with_nature.jpg?" alt="nature"></img>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      </div> */}
     </div>
   );
 }
